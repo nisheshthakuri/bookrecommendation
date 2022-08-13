dfa6a886f9fa2e47078bbf68f25b0777f17a22d9
nishesh99 <nisheshthakuri06@gmail.com>
2022-08-13 13:34:09 +0545

    For reference

---
+++ mainapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from mainapp.helpers import get_book_title
+
+import BookRecommendation.settings as settings
+import pandas as pd
+import os
+
+book_path = os.path.join(settings.STATICFILES_DIRS[0] + '/mainapp/dataset/books.csv')
+df_book = pd.read_csv(book_path)
+
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return f'{self.user.username} Profile'
+
+
+class UserRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_rating')
+    bookid = models.IntegerField()
+    bookrating = models.IntegerField()
+
+    def __str__(self):
+        return self.user.username.capitalize() + '- ' + get_book_title(self.bookid) + '  - ' + str(self.bookrating)
+
